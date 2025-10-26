6da364f244838d7556f42ab11b1c93d7c6e56608
JorgeRico <jorge.rico.garriga@gmail.com>
2025-10-26 23:55:16 +0100

    hotfix: rename schemas

--- schemas/league.js
+++ schemas/league.js
@@ -1,6 +1,6 @@
 import { z } from 'zod';
 
-const CardSchema = z.object({
+const LeagueSchema = z.object({
     name    : z.string("Name must be a string").min(3, "Name must be at least 3 characters long"),
     year    : z.number().int().positive().min(2000, "Year must be a valid year"),
     current : z.boolean("Current must be a boolean"),
@@ -8,7 +8,7 @@ const CardSchema = z.object({
 });
 
 export const validateLeague = (object) => {
-    return CardSchema.safeParse(object);
+    return LeagueSchema.safeParse(object);
 }
 
 const LeagueIdSchema = z.number().int().positive("ID must be a positive integer");
