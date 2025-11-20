2a5c5ab50a94cb5cdf49606e62c2071a76d1fad1
JorgeRico <jorge.rico.garriga@gmail.com>
2025-11-20 02:34:38 +0100

    feature: cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -3,26 +3,28 @@ import cors from 'cors';
 const ACCEPTED_ORIGINS = [
   'https://mtg-top8-backoffice-front.vercel.app',
   'https://mtg-top8-backoffice-front.vercel.app/',
+  'mtg-top8-backoffice-1hluiegql-jorge-ricos-projects.vercel.app',
   'http://localhost:5173',
   'http://127.0.0.1:5173',
   'http://127.0.0.1',
 ];
 
 export const corsMiddleware = ( { acceptedOrigins = ACCEPTED_ORIGINS } = {}) => cors({
-    origin: (origin, callback) => {
+    origin: '*',
+    // (origin, callback) => {
 
-        console.log(origin)
-        console.log(acceptedOrigins)
-        if (!origin) {
-            return callback(null, true); 
-        }
+    //     console.log(origin)
+    //     console.log(acceptedOrigins)
+    //     if (!origin) {
+    //         return callback(null, true); 
+    //     }
 
-        if (acceptedOrigins.includes(origin)) {
-            return callback(null, true);       
-        }
+    //     if (acceptedOrigins.includes(origin)) {
+    //         return callback(null, true);       
+    //     }
 
-        return callback(new Error('Not allowed by CORS'));
-    },
+    //     return callback(new Error('Not allowed by CORS'));
+    // },
     credentials: true,
     methods: "GET,HEAD,PUT,POST,DELETE",
     preflightContinue: false,
