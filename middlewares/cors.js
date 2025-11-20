fafe8500f7dfe2b9fa6dd84866471d3251b8ddce
JorgeRico <jorge.rico.garriga@gmail.com>
2025-11-20 01:46:22 +0100

    feature: vercel cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,7 +1,7 @@
 import cors from 'cors';
 
 const ACCEPTED_ORIGINS = [
-  // 'http://example.com',
+  'https://mtg-top8-backoffice-front.vercel.app',
   'http://localhost:5173',
   'http://127.0.0.1:5173',
   'http://127.0.0.1',
