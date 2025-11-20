46ca11ef077625e354397396d10cd56ac1e49151
JorgeRico <jorge.rico.garriga@gmail.com>
2025-11-20 02:16:27 +0100

    feature: vercel package config

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -2,6 +2,7 @@ import cors from 'cors';
 
 const ACCEPTED_ORIGINS = [
   'https://mtg-top8-backoffice-front.vercel.app',
+  'https://mtg-top8-backoffice-front.vercel.app/',
   'http://localhost:5173',
   'http://127.0.0.1:5173',
   'http://127.0.0.1',
@@ -19,5 +20,8 @@ export const corsMiddleware = ( { acceptedOrigins = ACCEPTED_ORIGINS } = {}) => 
 
         return callback(new Error('Not allowed by CORS'));
     },
-    credentials: true
+    credentials: true,
+    methods: "GET,HEAD,PUT,POST,DELETE",
+    preflightContinue: false,
+    optionsSuccessStatus: 204
 });
