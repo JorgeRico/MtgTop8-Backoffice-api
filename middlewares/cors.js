65bf09aeb685197e3ed0b6bbbfae2a513a1ba8fc
JorgeRico <jorge.rico.garriga@gmail.com>
2025-12-05 15:43:48 +0100

    feature: package updates + local cors change

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 const ACCEPTED_ORIGINS = [
   'https://mtg-top8-backoffice-front.vercel.app',
   'https://mtg-top8-backoffice-front.vercel.app/',
+  'http://localhost:5174',
   'http://localhost:5173',
   'http://127.0.0.1:5173',
   'http://127.0.0.1',
